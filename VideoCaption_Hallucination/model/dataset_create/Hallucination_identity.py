799697b11c74343ec0b793511727e70165085e55
no-zjc <54884338+no-zjc@users.noreply.github.com>
2024-04-18 00:35:37 -0400

    Update Hallucination_identity.py

--- VideoCaption_Hallucination/model/dataset_create/Hallucination_identity.py
+++ VideoCaption_Hallucination/model/dataset_create/Hallucination_identity.py
@@ -12,6 +12,7 @@ import clip
 import h5py
 from tqdm import tqdm
 
+from VideoCaption_Hallucination.model.Net_Utils import Conv1D
 from VideoCaption_Hallucination.model.dataset_create.hu_dataset_tool import load_data_for_training_HU
 import torch
 import torch.nn as nn
@@ -33,6 +34,119 @@ clip_model, feature_extractor = clip.load("ViT-L/14", device=device, download_ro
 num_epochs = 20
 batch_size = 10
 
+
+class Cross_Attention_Module(nn.Module):
+    def __init__(self, cross_attention_reduce_factor=4, embed_dim=768, num_heads=12):
+        super().__init__()
+        self.cross_attention_reduce_factor = cross_attention_reduce_factor
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = int(self.embed_dim / self.cross_attention_reduce_factor / self.num_heads)
+
+        self.q_attn = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+        self.k_attn = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+        self.v_attn = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+
+        self.c_proj = Conv1D(self.embed_dim, int(self.embed_dim / self.cross_attention_reduce_factor))
+
+        self.q_attn_vtt = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+        self.k_attn_vtt = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+        self.v_attn_vtt = Conv1D(int(self.embed_dim / self.cross_attention_reduce_factor), self.embed_dim)
+
+        self.c_proj_vtt = Conv1D(self.embed_dim, int(self.embed_dim / self.cross_attention_reduce_factor))
+        # self.c_attn = Conv1D(int(2 / self.cross_attention_reduce_factor * self.embed_dim), self.embed_dim)
+
+    def _split_heads(self, tensor, num_heads, attn_head_size):
+        """
+        Splits hidden_size dim into attn_head_size and num_heads
+        """
+        new_shape = tensor.size()[:-1] + (num_heads, attn_head_size)
+        tensor = tensor.view(new_shape)
+        return tensor.permute(0, 2, 1, 3)  # (batch, head, seq_length, head_features)
+    def _attn(self, query, key, value, attention_mask=None, head_mask=None):
+        attn_weights = torch.matmul(query, key.transpose(-1, -2))
+
+        if attention_mask is not None:
+            # Apply the attention mask
+            attn_weights = attn_weights + attention_mask
+
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+        # Downcast (if necessary) back to V's dtype (if in mixed-precision) -- No-Op otherwise
+        attn_weights = attn_weights.type(value.dtype)
+
+        # attn_weights = self.attn_dropout(attn_weights)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attn_weights = attn_weights * head_mask
+
+        attn_output = torch.matmul(attn_weights, value)
+        return attn_output, attn_weights
+
+    def _merge_heads(self, tensor, num_heads, attn_head_size):
+        """
+        Merges attn_head_size dim and num_attn_heads dim into hidden_size
+        """
+        tensor = tensor.permute(0, 2, 1, 3).contiguous()
+        new_shape = tensor.size()[:-2] + (num_heads * attn_head_size,)
+        return tensor.view(new_shape)
+
+    def forward(self, input_tensor):
+        # batch_size, num_heads, embedding_size = input_tensor.size()
+        cap_fts = input_tensor[:, :1, :]
+        video_fts = input_tensor[:, 1:, :]
+
+        query = self.q_attn(cap_fts)
+        key = self.k_attn(video_fts)
+        value = self.v_attn(video_fts)
+
+        query = self._split_heads(query, self.num_heads, self.head_dim)
+        key = self._split_heads(key, self.num_heads, self.head_dim)
+        value = self._split_heads(value, self.num_heads, self.head_dim)
+
+        attn_output, attn_weights = self._attn(query, key, value)
+
+        attn_output = self._merge_heads(attn_output, self.num_heads, self.head_dim)
+        attn_output = self.c_proj(attn_output)
+
+        query_vtt = self.q_attn_vtt(video_fts)
+        key_vtt = self.k_attn_vtt(cap_fts)
+        value_vtt = self.v_attn_vtt(cap_fts)
+
+        query_vtt = self._split_heads(query_vtt, self.num_heads, self.head_dim)
+        key_vtt = self._split_heads(key_vtt, self.num_heads, self.head_dim)
+        value_vtt = self._split_heads(value_vtt, self.num_heads, self.head_dim)
+
+        attn_output_vtt, attn_weights_vtt = self._attn(query_vtt, key_vtt, value_vtt)
+
+        attn_output_vtt = self._merge_heads(attn_output_vtt, self.num_heads, self.head_dim)
+        attn_output_vtt = self.c_proj(attn_output_vtt)
+
+        outputs = torch.cat((attn_output, attn_output_vtt), dim=1)
+
+        return outputs  # a, present, (attentions)
+class HallucinationIdentityModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(HallucinationIdentityModel, self).__init__()
+        self.multi_model_cross_attention_layer = Cross_Attention_Module()
+        self.transformer_layer1 = nn.TransformerEncoderLayer(input_size, nhead=8)
+        self.transformer_layer2 = nn.TransformerEncoderLayer(input_size, nhead=8)
+        self.layerNorm = nn.LayerNorm(input_size)
+        self.mlp_layer = nn.Linear(input_size, hidden_size)
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.multi_model_cross_attention_layer(x)
+        x = self.layerNorm(x)
+        x = self.transformer_layer1(x)
+        x = self.transformer_layer2(x)
+        x = torch.mean(x, dim=1)
+        x = self.layerNorm(x)
+        x = self.mlp_layer(x)
+        x = self.output_layer(x)
+        x = self.softmax(x)
+        return x
 
 def get_features_labels(split_text, clip_model, a_path, fts_path):
     data = load_data_for_training_HU(a_path)
@@ -104,27 +218,6 @@ def evaluate_model(test_hu_features, test_hu_labels, eval_model):
     return acc_dict
 
 
-class HallucinationIdentityModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(HallucinationIdentityModel, self).__init__()
-        self.transformer_layer1 = nn.TransformerEncoderLayer(input_size, nhead=8)
-        self.transformer_layer2 = nn.TransformerEncoderLayer(input_size, nhead=8)
-        self.layerNorm = nn.LayerNorm(input_size)
-        self.mlp_layer = nn.Linear(input_size, hidden_size)
-        self.output_layer = nn.Linear(hidden_size, num_classes)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = self.layerNorm(x)
-        x = self.transformer_layer1(x)
-        x = self.transformer_layer2(x)
-        x = torch.mean(x, dim=1)
-        x = self.layerNorm(x)
-        x = self.mlp_layer(x)
-        x = self.output_layer(x)
-        x = self.softmax(x)
-        return x
-
 def train_model():
     train_hu_data, train_hu_features, train_hu_labels = get_features_labels("train", clip_model, annotations_path[dt], data_fts_path[dt])
     val_hu_data, val_hu_features, val_hu_labels = get_features_labels("val", clip_model, annotations_path[dt], data_fts_path[dt])
@@ -174,7 +267,7 @@ def train_model():
 
 if __name__ == '__main__':
 
-    # train_model()
+    train_model()
 
     # 创建模型实例
     eval_model = HallucinationIdentityModel(input_size=768, hidden_size=256, num_classes=3)
